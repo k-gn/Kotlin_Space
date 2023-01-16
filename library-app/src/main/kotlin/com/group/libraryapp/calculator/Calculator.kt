bd27eb2bd97c815c65e5730a1aa95ece3f8ed2e8
k-gn <rlarbska97@gmail.com>
2023-01-16 10:16:37 +0900

    feat: 수동으로 코틀린 테스트 코드 작성해보기

---
+++ library-app/src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
@@ -0,0 +1,29 @@
+package com.group.libraryapp.calculator
+
+class Calculator(
+    var number: Int,
+) {
+
+    // backing property
+    //val number: Int
+    //    get() = this._number
+
+    fun add(operand: Int) {
+        this.number += operand
+    }
+
+    fun minus(operand: Int) {
+        this.number -= operand
+    }
+
+    fun multiply(operand: Int) {
+        this.number *= operand
+    }
+
+    fun divide(operand: Int) {
+        if (operand == 0) {
+            throw IllegalArgumentException("0으로 나눌 수 없습니다.")
+        }
+        this.number /= operand
+    }
+}
