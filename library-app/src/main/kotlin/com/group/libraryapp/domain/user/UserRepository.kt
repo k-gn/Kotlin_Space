52f447967630c7b33bdccd46972870ad80dd7783
k-gn <rlarbska97@gmail.com>
2023-01-22 21:35:55 +0900

    feat: querydsl 적용

--- library-app/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -3,11 +3,11 @@ package com.group.libraryapp.domain.user
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
-interface UserRepository : JpaRepository<User, Long> {
+interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom {
 
     fun findByName(name: String): User?
 
     // fetch join 으로 N+1 이슈 해결하기
-    @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.userLoanHistories")
-    fun findAllWithHistories(): List<User>
+    // @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.userLoanHistories")
+    // fun findAllWithHistories(): List<User>
 }
