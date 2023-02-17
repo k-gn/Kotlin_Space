41dac262db39dbadd8620970589223b0905afe67
k-gn <rlarbska97@gmail.com>
2023-02-17 10:50:08 +0900

    feat: kotlin project init

---
+++ toppings_kotlin/toppings/src/main/kotlin/kr/co/toppings/ToppingsApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.toppings
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToppingsApplication
+
+fun main(args: Array<String>) {
+    runApplication<ToppingsApplication>(*args)
+}
---
+++ toppings_kotlin/toppings/src/test/kotlin/kr/co/toppings/ToppingsApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.toppings
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToppingsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
