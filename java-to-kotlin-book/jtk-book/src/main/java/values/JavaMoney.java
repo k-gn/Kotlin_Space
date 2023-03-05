a10c877c225ff6148943df4b8f2203b29d157e24
k-gn <rlarbska97@gmail.com>
2023-03-05 20:48:41 +0900

    fix: update java class name

--- java-to-kotlin-book/jtk-book/src/main/java/values/JavaMoney.java
+++ java-to-kotlin-book/jtk-book/src/main/java/values/JavaMoney.java
@@ -4,18 +4,18 @@ import java.math.BigDecimal;
 import java.util.Currency;
 import java.util.Objects;
 
-public class Money {
+public class JavaMoney {
 
 	private final BigDecimal amount;
 	private final Currency currency;
 
-	private Money(BigDecimal amount, Currency currency) { // <1> private constructor
+	private JavaMoney(BigDecimal amount, Currency currency) { // <1> private constructor
 		this.amount = amount;
 		this.currency = currency;
 	}
 
-	public static Money of(BigDecimal amount, Currency currency) { // <1> 정적 팩터리 메서드
-		return new Money(
+	public static JavaMoney of(BigDecimal amount, Currency currency) { // <1> 정적 팩터리 메서드
+		return new JavaMoney(
 			/*
 				BigDecimal 의 setScale 함수는 setter 이름이지만 실제론 상태를 변경하지 않아 혼란을 야기할 수 있다.
 				따라서 withScale 같은 이름의 확장함수를 추가해 바로잡을 수 있다.
@@ -25,16 +25,16 @@ public class Money {
 	}
 
 
-	public static Money of(String amountStr, Currency currency) { // <2> 정적 팩터리 메서드
-		return Money.of(new BigDecimal(amountStr), currency);
+	public static JavaMoney of(String amountStr, Currency currency) { // <2> 정적 팩터리 메서드
+		return JavaMoney.of(new BigDecimal(amountStr), currency);
 	}
 
-	public static Money of(int amount, Currency currency) {
-		return Money.of(new BigDecimal(amount), currency);
+	public static JavaMoney of(int amount, Currency currency) {
+		return JavaMoney.of(new BigDecimal(amount), currency);
 	}
 
-	public static Money zero(Currency userCurrency) {
-		return Money.of(BigDecimal.ZERO, userCurrency);
+	public static JavaMoney zero(Currency userCurrency) {
+		return JavaMoney.of(BigDecimal.ZERO, userCurrency);
 	}
 
 
@@ -50,7 +50,7 @@ public class Money {
 	public boolean equals(Object o) {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
-		Money money = (Money) o;
+		JavaMoney money = (JavaMoney) o;
 		return amount.equals(money.amount) &&
 			currency.equals(money.currency);
 	}
@@ -65,12 +65,12 @@ public class Money {
 		return amount.toString() + " " + currency.getCurrencyCode();
 	}
 
-	public Money add(Money that) { // <4> add calculator
+	public JavaMoney add(JavaMoney that) { // <4> add calculator
 		if (!this.currency.equals(that.currency)) {
 			throw new IllegalArgumentException(
 				"cannot add Money values of different currencies");
 		}
 
-		return new Money(this.amount.add(that.amount), this.currency);
+		return new JavaMoney(this.amount.add(that.amount), this.currency);
 	}
 }
