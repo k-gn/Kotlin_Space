70b69302d15b76f4d977e88780c04b56789008a1
k-gn <rlarbska97@gmail.com>
2023-08-08 19:09:58 +0900

    feat: kotlin class

---
+++ kotlinlang/src/basics/Classes.kt
@@ -0,0 +1,13 @@
+package basics
+
+class Customer
+
+class Contact(val id: Int, var email: String)
+
+fun main() {
+    val customer = Customer()
+    val contact = Contact(1, "mary@gmail.com")
+
+    println(contact.id)
+    contact.email = "jane@gmail.com"
+}
